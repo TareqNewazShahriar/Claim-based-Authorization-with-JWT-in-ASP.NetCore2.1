c02321749a000b71781b6376b49f422a6ad4e4e0
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2018-07-25 14:40:27 +0600

    project done with server side + js login. session and coockie both used

---
+++ jwt_api_sample/jwt-sample/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using jwt_sample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwt_sample.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+        IConfiguration _config;
+
+        public AccountController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult GetAuthenticated([FromBody] UserModel model)
+        {
+            if(string.IsNullOrEmpty(model.Username)==false && string.IsNullOrEmpty(model.Password) == false)
+            {
+               string tokenStr = GenerateToken(model);
+                return Ok(tokenStr);
+            }
+
+            return Unauthorized();
+        }
+
+        private string GenerateToken(UserModel user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim> {
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            if (user.Dob.HasValue)
+                claims.Add(new Claim(JwtRegisteredClaimNames.Birthdate, user.Dob.Value.ToShortDateString()));
+
+            var tokenObj = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.UtcNow.AddDays(7),
+              signingCredentials: creds);
+
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(tokenObj);
+            return tokenStr;
+        }
+    }
+}
---
+++ jwt_api_sample/jwt-sample/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace jwt_sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        ClockSkew = TimeSpan.Zero /* no buffer time after expiration of token */
+
+                        //,LifetimeValidator =
+                        //    (DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters)
+                        //        =>
+                        //    { return true; }
+                    };
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+
+            // DEFINES Policy
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("HasDob", policy => policy.RequireClaim(claimType: System.Security.Claims.ClaimTypes.DateOfBirth));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(builder => builder
+               //.WithOrigins("*://localhost:44325/*")
+               .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication(); // must before UseMvc()
+            app.UseMvc().UseMvcWithDefaultRoute();
+        }
+    }
+}
